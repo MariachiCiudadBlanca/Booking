9166a5fb8bc8b08818c6a3c5d2968b2e831b0c96
MariachiCiudadBlanca <mariachiciudadblanca@gmail.com>
2024-08-09 00:51:27 -0500

    Initial

--- js/main.js
+++ js/main.js
@@ -2,3 +2,27 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('nav ul').classList.toggle('show');
 });
+
+
+/* Sección deslizador de imagenes que va en el home */
+// main.js
+document.addEventListener('DOMContentLoaded', function() {
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+    const slides = document.querySelector('.slides');
+    let index = 0;
+
+    function showSlide(n) {
+        const totalSlides = document.querySelectorAll('.slides img').length;
+        index = (n + totalSlides) % totalSlides;
+        slides.style.transform = `translateX(${-index * 100}%)`;
+    }
+
+    prevButton.addEventListener('click', function() {
+        showSlide(index - 1);
+    });
+
+    nextButton.addEventListener('click', function() {
+        showSlide(index + 1);
+    });
+});
